22bcb755e4eabf67b6615c3630762be8cb18f714
Krish Agarwal <krisha2@lovelace.ece.local.cmu.edu>
2025-09-21 14:17:43 -0400

    update

--- run_job.sh
+++ run_job.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 kubectl apply -f yaml/run_monarch_shared_LR.yaml
-kubectl apply -f yaml/run_monarch_shared_local.yaml
-kubectl apply -f yaml/run_monarch_max_constrain.yaml
-kubectl apply -f yaml/run_monarch_max_constrain_reuse_qkv.yaml
